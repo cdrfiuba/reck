e0180fe4ede452c6513d8a0fb8db35b0e6055541
Maximiliano Alexis Montiel <maximilianomontiel@gmail.com>
2016-02-13 01:35:25 -0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+/*
+  R.E.C.K.  Robot Eyes Caterpillar Kinect
+  Copyright (c) 2016 Club de Robótica - FIUBA
+
+  main.h
+    Main header del sistema R.E.C.K.
+    Aquí se incluirán todos los headers de los distintos componentes que se usen.
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "ros/ros.h"  // main header de ros
+
+#endif  // MAIN_H
